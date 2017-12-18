199a183bb12b619b0f3932bacd82f6ee6b0e313f
Wang <gavin19950511@gmail.com>
2017-12-19 00:09:43 +0800

    init
    initial

---
+++ Django/myproject/DemoProject/views.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from datetime import datetime
+from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+import sys
+sys.path.append('C:\\Users\\2017SEa\\Desktop\\Django_Template_python2-master\\Django\\myproject\\DemoProject\\models')
+import MySQL_model
+###### show view ######
+
+def hello_world(request):
+    template = 'hello_world.html'
+    responds = {'stringData': str(datetime.now()),}
+    return render(request,template,responds )
+
+
+def UsingStaticSource(request):
+    template = 'UsingStaticSource.html'
+    responds = {}
+    return render(request,template,responds )
+
+def For_Cycle(request):
+    template = 'For_Cycle.html'
+    arr = [1,2,3,4,5,6,7,8,9]
+    responds = {'Data':arr}
+    return render(request,template,responds )
+
+def Http_From_Get(request,input1,input2):
+    template = 'hello_world.html'
+    responds = {'stringData': str(input1)+str(input2),}
+    return render(request,template,responds )
+
+@csrf_exempt #csrf skip, if you want to get the http's post form anywhere
+def Http_From_Post(request):
+    try:
+        data = str(request.POST['input1'])
+    except KeyError as e:
+        data = 'did not have post'
+
+
+    template = 'Form.html'
+    responds = {'stringData': data,}
+    return render(request,template,responds )
+
+
+
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# Django_Template_python3
+# template for Django
+
+功能板模
+
+
+. 靜態網頁
+
+. 引用靜態資源
+
+. 迴圈網頁元件
+
+. Get網頁
+
+. Post網頁
+
+. 資料庫連接lib
+
